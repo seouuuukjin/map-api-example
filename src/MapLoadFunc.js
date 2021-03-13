a7a8dee45d4812fbba0c9cdd21538de2ce550f81
seouuuukjin <uukjin4@gmail.com>
2021-03-13 23:57:57 +0900

    이제 지도 검색 기능 추가필요

--- src/MapLoadFunc.js
+++ src/MapLoadFunc.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components'
 
 const { kakao } = window;
@@ -10,7 +10,19 @@ const MapLoadFunc = () => {
 			center: new kakao.maps.LatLng(33.450701, 126.570667),
 			level: 3
 		};
+        //이 문장이 map 을 띄워준다.
         const map = new kakao.maps.Map(container, options);
+
+        //resize 이벤트 감지용 임시 함수
+        const handleResize = () => {
+            console.log(`브라우저 화면 사이즈 x: ${window.innerWidth}, y: ${window.innerHeight}`);
+        }
+        //window resize 이벤트 감지 위해서 추가
+        window.addEventListener('resize', handleResize);
+        return () => { // cleanup 
+            window.removeEventListener('resize', handleResize);
+        }
+
     }, []);
 
     return (
